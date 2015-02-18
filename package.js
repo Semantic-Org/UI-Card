5add67aa9efee3f3761a72ec5e4dbdfedbc7e78a
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:32:14 -0500

    Updated component to version 1.9.1

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-card',
+  summary : 'Semantic UI - Card (official): Single component release of card',
+  version : '1.9.1',
+  git     : 'git://github.com/Semantic-Org/UI-Card.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});

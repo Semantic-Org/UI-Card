1fa87cbe5741ab6a58c2e12ec48ca6621a143b01
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:15:29 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,14 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-card',
-  summary : 'Semantic UI - Card (official): Single component release of card',
-  version : '1.9.2',
+  summary : 'Semantic UI - Card: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Card.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'card.css'
+  ], 'client');
 });

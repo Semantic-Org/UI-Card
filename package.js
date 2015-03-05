7834af3e5ae17f003d4566bf947d1295ec86fc29
Pusher Robot <jack@semantic-ui.com>
2015-03-05 11:03:15 -0500

    Updated component to version 1.11.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-card',
   summary : 'Semantic UI - Card: Single component release',
-  version : '1.10.3',
+  version : '1.11.0',
   git     : 'git://github.com/Semantic-Org/UI-Card.git',
 });
 

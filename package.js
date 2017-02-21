e8155c7cd0b978484066d89cc30f9b8424737b1e
Jack Lukic <jack@semantic-ui.com>
2017-02-21 14:22:55 -0500

    Updated component to version 2.2.9

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-card',
   summary : 'Semantic UI - Card: Single component release',
-  version : '2.2.8',
+  version : '2.2.9',
   git     : 'git://github.com/Semantic-Org/UI-Card.git',
 });
 
